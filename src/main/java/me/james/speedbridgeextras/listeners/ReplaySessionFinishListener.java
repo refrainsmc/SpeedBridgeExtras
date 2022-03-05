6eecd0ac6cba402ee85ba21c1699e429d1ae88e9
James <james@jamesblair.nz>
2022-03-05 21:21:04 +1300

    fix bug

--- src/main/java/me/james/speedbridgeextras/listeners/ReplaySessionFinishListener.java
+++ src/main/java/me/james/speedbridgeextras/listeners/ReplaySessionFinishListener.java
@@ -1,12 +1,10 @@
 package me.james.speedbridgeextras.listeners;
 
 import me.james.speedbridgeextras.SpeedBridgeExtras;
-import me.jumper251.replay.api.ReplayAPI;
 import me.jumper251.replay.api.ReplaySessionFinishEvent;
 import me.tofpu.speedbridge.api.SpeedBridgeAPI;
 import me.tofpu.speedbridge.api.user.User;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,9 +18,9 @@ public class ReplaySessionFinishListener implements Listener {
     public void onPlayerReplayFinish(ReplaySessionFinishEvent e) {
         Player player = e.getPlayer();
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            User speedBridgeUser = SpeedBridgeAPI.getUserService().get(player.getUniqueId());
             player.removeMetadata("currentlyWatching", plugin);
             SpeedBridgeAPI.getGameService().leave(player);
-        }, 1L);
+            player.teleport(SpeedBridgeAPI.getLobbyService().getLobbyLocation());
+        }, 2L);
     }
 }
